65bb72209bea2f9317bedaada2346e237a67bf84
AkLabx <Dhiraja129@gmail.com>
2026-02-26 08:53:14 +0530

    Remove Quill CSS import from index.tsx
    Removed import statement for Quill CSS.

--- index.tsx
+++ index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'react-quill/dist/quill.snow.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
